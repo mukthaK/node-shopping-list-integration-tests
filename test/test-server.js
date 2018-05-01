d8de40c9d9ada0ad3b54f6f1a6eb6d8bed6ba7d8
Muktha Krishnaswamy <muktha.1689@gmail.com>
2018-04-30 17:31:59 -0700

    added test scripst for recipes

--- test/test-server.js
+++ test/test-server.js
@@ -142,3 +142,85 @@ describe('Shopping List', function() {
       });
   });
 });
+
+//Test cases for Recipes
+describe('recipes', function() {
+  before(function() {
+    return runServer();
+  });
+  after(function() {
+    return closeServer();
+  });
+  it('should list items on GET', function() {
+    return chai.request(app)
+      .get('/recipes')
+      .then(function(res) {
+        expect(res).to.have.status(200);
+        expect(res).to.be.json;
+        expect(res.body).to.be.a('array');
+
+        // because we create three items on app load
+        expect(res.body.length).to.be.at.least(1);
+        // each item should be an object with key/value pairs
+        // for `id`, `name` and `checked`.
+        const expectedKeys = ['id', 'name', 'ingredients'];
+        res.body.forEach(function(item) {
+          expect(item).to.be.a('object');
+          expect(item).to.include.keys(expectedKeys);
+        });
+      });
+  });
+  it('should add an item on POST', function() {
+    const newItem = {name: 'coffee', ingredients: ['milk','coffee powder', 'sugar']};
+    return chai.request(app)
+      .post('/recipes')
+      .send(newItem)
+      .then(function(res) {
+        expect(res).to.have.status(201);
+        expect(res).to.be.json;
+        expect(res.body).to.be.a('object');
+        expect(res.body).to.include.keys('id', 'name', 'ingredients');
+        expect(res.body.id).to.not.equal(null);
+        expect(res.body).to.deep.equal(Object.assign(newItem, {id: res.body.id}));
+      });
+  });
+  it('should update items on PUT', function() {
+    // we initialize our updateData here and then after the initial
+    // request to the app, we update it with an `id` property so
+    // we can make a second, PUT call to the app.
+    const updateData = {
+      name: 'coffee',
+      ingredients: ['milk','coffee powder', 'sugar', 'water']
+    };
+
+    return chai.request(app)
+      // first have to get so we have an idea of object to update
+      .get('/recipes')
+      .then(function(res) {
+        updateData.id = res.body[0].id;
+
+        return chai.request(app)
+          .put(`/recipes/${updateData.id}`)
+          .send(updateData);
+      })
+      .then(function(res) {
+        expect(res).to.have.status(200);
+        expect(res).to.be.json;
+        expect(res.body).to.be.a('object');
+        expect(res.body).to.deep.equal(updateData);
+      });
+  });
+  it('should delete items on DELETE', function() {
+    return chai.request(app)
+      // first have to get so we have an `id` of item
+      // to delete
+      .get('/recipes')
+      .then(function(res) {
+        return chai.request(app)
+          .delete(`/recipes/${res.body[0].id}`);
+      })
+      .then(function(res) {
+        expect(res).to.have.status(204);
+      });
+  });
+});
